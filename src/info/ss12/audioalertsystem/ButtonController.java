ef5356b29e212405806705e2a2158144fec6e0aa
Jon <kyoeng.jung@gmai.com>
2013-02-09 16:32:07 -0800

    working on branch

--- src/info/ss12/audioalertsystem/ButtonController.java
+++ src/info/ss12/audioalertsystem/ButtonController.java
@@ -1,67 +1,80 @@
 package info.ss12.audioalertsystem;
 
+import android.content.Context;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
-import android.widget.CompoundButton;
 import android.widget.Switch;
 
 public class ButtonController implements OnClickListener, OnTouchListener
 {
 	private final String TAG = "Button Controller";
-
+	private Context mainActivity;
+	
+	public ButtonController(Context mainActivity)
+	{
+		this.mainActivity = mainActivity;
+	}
+	
 	@Override
 	public void onClick(View v)
 	{
+		Log.d(TAG, "On CLICK CALLED");
 		handleButtonToggle(v);
 	}
 
 	@Override
 	public boolean onTouch(View v, MotionEvent event)
 	{
-		int eventId = event.getAction();
-
-		if (eventId == MotionEvent.ACTION_UP)
-		{
-			//For UI purposes
-			if(v instanceof Switch)
-			{
-				Switch temp = (Switch) v;
-				if(temp.isChecked())
-					temp.setChecked(false);
-				else
-					temp.setChecked(true);
-			}
-			handleButtonToggle(v);
-			return true;
-		}
+//		Log.d(TAG, "On TOUCH CALLED");
+//		int eventId = event.getAction();
+//
+//		if (eventId == MotionEvent.ACTION_MOVE)
+//		{
+//			//For UI purposes
+//			if(v instanceof Switch)
+//			{
+//				Switch temp = (Switch) v;
+//				if(temp.isChecked())
+//					temp.setChecked(false);
+//				else
+//					temp.setChecked(true);
+//			}
+//			handleButtonToggle(v);
+//			return true;
+//		}
 		return false;
 	}
 
 	public void handleButtonToggle(View v)
 	{
 		int id = v.getId();
-		switch (id)
+
+		
+		if(id == R.id.mic_switch)
 		{
-		case R.id.mic_switch:
 			Log.d(TAG, "press");
 			Switch micS = (Switch) v;
 
 			if (micS.isChecked())
+			{
 				Log.d(TAG, "switch on");
+				DetectorServiceHelper.startServices(mainActivity);
+			}
 			else
+			{
 				Log.d(TAG, "switch off");
-
-			break;
-
-		
+				DetectorServiceHelper.stopServices();
+			}
 			
-		default:
-			Log.d("swtich button test", "default");
-			break;
+			
 		}
+		
+		
+		
+		
 	}
 
 }
